a1de922a20383c933fb71b22b99737eb5e38afdb
khoa <hatgiong@gmail.com>
2021-10-22 19:26:54 +0700

    Initial commit

---
+++ src/Buoi1.java
@@ -0,0 +1,21 @@
+public class Buoi1 {
+    public static void main(String[] args) {
+    /*
+        String name = " Phạm Đăng Khoa";
+    System.out.println(name);
+    int tuoi = 37;
+    String address = " Tây Ninh";
+    boolean isMale = true;
+
+    float height = 1.75f;
+//    System.out.println(height);
+//    System.out.println("Tên:" + name);
+//    System.out.println("Tuổi: " + tuoi);
+//    System.out.println("Địa chỉ : " + address);
+//    System.out.println("Giới Tính: " + isMale);
+//    System.out.println("Chiều cao" + height);
+*/
+        
+
+    }
+}
